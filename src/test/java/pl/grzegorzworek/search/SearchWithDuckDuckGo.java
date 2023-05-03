eda9b61f602a9e77db849ddfc2f00684188ff604
grzesiek-worek <grzesiekw@interia.eu>
2023-05-03 10:09:58 +0200

    add search with duckduckgo example

---
+++ src/test/java/pl/grzegorzworek/search/SearchWithDuckDuckGo.java
@@ -0,0 +1,20 @@
+package pl.grzegorzworek.search;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SearchWithDuckDuckGo {
+    @Test
+    public void searchWithDuckDuckGo() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://duckduckgo.com/");
+        WebElement searchInput = driver.findElement(By.id("search_form_input_homepage"));
+        searchInput.sendKeys("W pustyni i w puszczy");
+        WebElement searchButton = driver.findElement(By.id("search_button_homepage"));
+        searchButton.click();
+        driver.quit();
+    }
+}
